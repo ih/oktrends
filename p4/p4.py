db23148c949758a4fa2a643b3154a8ef0ca648a6
irvin <ih@wizard.(none)>
2010-04-13 12:18:09 -0400

    working on extend rectangle; still need to take care of special cases for neighbor and optimize populate

--- p4/p4.py
+++ p4/p4.py
@@ -1,6 +1,7 @@
 import sys
 from sqlalchemy import create_engine
 from Queue import PriorityQueue
+from copy import copy
 import pdb
 
 rectangleNum = 5
@@ -8,6 +9,7 @@ rectangles = []
 areaLimit = 100 #square miles
 meshSize = 1 #square miles
 seedLimit = areaLimit/meshSize #the most number of seeds one might need
+allUsers = []
 def preprocess(dbuser,dbpass):
     s='mysql://'+dbuser+':'+dbpass+'@localhost/first'
 #    engine = create_engine('mysql://'+dbuser+':'+dbpass+'+@localhost/first')
@@ -18,6 +20,7 @@ def preprocess(dbuser,dbpass):
     east=Rectangle(Coord(90,0),Coord(-90,180))
     for row in data:
         u=User(row[0],row[1],row[2])
+        allUsers.append(u) #change this to insert user into two lists sorted by lat and lon; eventually used in populate
         if u.pos.lon<=0:
             west.addUser(u)
         else:
@@ -27,27 +30,71 @@ def preprocess(dbuser,dbpass):
     rq.put(west)
     return rq
 
-
 def findRectangles(dbuser,dbpass):
     #find squares with area meshSize that are most populated
     seeds = findSeeds(preprocess(dbuser,dbpass)) #a collection with a delete function
     while len(rectangles) < rectangleNum and seeds:
         # mostPopulated=popMostPopulated(seeds) 
-        seed=popMostPopulated(seeds)
+        seed=seeds.pop(0)
         growthQueue=UniqueRectangleQueue()
-        growQueue.put(seed)
-        # hypRec is our hypothesis rectangle that grows to cover the most populated seeds
+        growthQueue.put(seed)
+        # hypRec is our hypothesis rectangle that gets extended to cover the most populated neighboring squares
         hypRec = copy(seed) 
         while area(hypRec)<areaLimit and growthQueue:
             mostPopulated = growthQueue.get()
             for neighbor in neighbors(mostPopulated):
                 try: 
-                    seeds.remove(neighbor)
+                    seeds.remove(neighbor) #populating neighbors can be optimized by using the already populated seeds if the seed is a neighbor
                 except ValueError:
                     None
                 growthQueue.put(neighbor)
             hypRec.extendRectangle(mostPopulated)
         rectangles.append(hypRec)
+
+def populate(rectangle,population):
+    #highly inefficient (can be improved by using two lists of users sorted by lat and lon respectively
+    for user in population:
+        assign(user, [rectangle])
+
+def neighbors(rectangle):
+    """return neighboring rectangles with the users who exist in them"""
+    assert area(rectangle) == meshSize
+    ns = [north(rectangle),west(rectangle),east(rectangle),south(rectangle)]
+    [populate(n,allUsers) for n in ns]
+    return ns
+
+def north(rectangle):
+    if rectangle.nw.lat+rectangle.height <= 90:
+        nw=Coord(rectangle.nw.lat+rectangle.height, rectangle.nw.lon)
+        se=Coord(rectangle.nw.lat, rectangle.se.lon)
+        return Rectangle(nw,se)
+    else:
+        assert rectangle.nw.lat == 90
+        assert False
+def west(rectangle):
+    if rectangle.nw.lon-rectangle.width >= -180:
+        nw=Coord(rectangle.nw.lat, rectangle.nw.lon-rectangle.width)
+        se=Coord(rectangle.se.lat, rectangle.nw.lon)
+        return Rectangle(nw,se)
+    else:
+        assert rectangle.nw.lon == -180
+        assert False
+def east(rectangle):
+    if rectangle.se.lon+rectangle.width <= 180:
+        nw=Coord(rectangle.nw.lat, rectangle.se.lon)
+        se=Coord(rectangle.se.lat, rectangle.se.lon+rectangle.width)
+        return Rectangle(nw,se)
+    else:
+        assert rectangle.se.lon == 180
+        assert False
+def south(rectangle):
+    if rectangle.se.lat-rectangle.height >= -90:
+        nw=Coord(rectangle.se.lat, rectangle.nw.lon)
+        se=Coord(rectangle.se.lat-rectangle.height, rectangle.se.lon)
+        return Rectangle(nw,se)
+    else:
+        assert rectangle.se.lat == -90
+        assert False
 
 def findSeeds(rectangleQueue):
     """return most populated squares of size dependent on mesh_size"""
@@ -64,7 +111,6 @@ def findSeeds(rectangleQueue):
             for rectangle in newRectangles:
                 if rectangle.popSize()>0:
                     rectangleQueue.put(rectangle)
-
 #        pdb.set_trace()
     return seeds
 
@@ -92,9 +138,7 @@ def assign(user, disjointRectangles):
 def isIn(user,rectangle):
     return user.pos.lat<=rectangle.nw.lat and user.pos.lat>rectangle.se.lat and user.pos.lon <= rectangle.se.lon and user.pos.lon > rectangle.nw.lon
 
-class UniqueRectangleQueue():
-    """A priority queue for rectangles that can only have a rectangle """
-
+            
 class RectangleQueue(PriorityQueue):
     def put(self,rectangle):
         #we use the negative b/c priority queues are implemented as min heaps in python
@@ -103,22 +147,42 @@ class RectangleQueue(PriorityQueue):
         (popNum,rectangle) = PriorityQueue.get(self)
         return rectangle
 
+class UniqueRectangleQueue(RectangleQueue):
+    """A priority queue that can only have a rectangle inserted once"""
+    def __init__(self):
+        RectangleQueue.__init__(self)
+        self.History = set()
+    def put(self,rectangle):
+        if rectangle not in self.History:
+            self.History.add(rectangle)
+            RectangleQueue.put(self, rectangle)
+
+
 class Rectangle:
     def __init__(self, northWest, southEast):
         self.nw = northWest
         self.se = southEast
+        self.height = northWest.lat-southEast.lat
+        assert(self.height > 0)
+        self.width = southEast.lon-northWest.lon
+        assert(self.width > 0)
         self.users = []
+
     def __repr__(self):
         return str(self.nw)+","+str(self.se)+" Contains:" + str(len(self.users))
+    def __eq__(self, other):
+        return self.nw == other.nw and self.se == other.se
+    def __hash__(self):
+        return hash((self.nw,self.se))
     def addUser(self,user):
         self.users.append(user)
     def popSize(self):
         return len(self.users)
+    def extenRectangle(self, expansion):
+        case 
 
 def area(rectangle):
-    length=(rectangle.se.lon-rectangle.nw.lon)*68.9722023
-    height=(rectangle.nw.lat-rectangle.se.lat)*68.9722023
-    return length*height
+    return rectangle.height*rectangle.width
 
 class Coord:
     def __init__(self, lat, lon):
@@ -126,6 +190,10 @@ class Coord:
         self.lon=lon
     def __repr__(self):
         return "("+str(self.lat)+","+str(self.lon)+")"
+    def __eq__(self, other):
+        return self.lat == other.lat and self.lon == other.lon
+    def __hash__(self):
+        return hash((self.lat,self.lon))
 
 class User:
     def __init__(self, userid, latitude, longitude):
--- p4/p4test.py
+++ p4/p4test.py
@@ -1,2 +1,28 @@
-import p4
+from p4 import *
 import unittest
+from copy import copy
+
+p1=Coord(100,0)
+p2=Coord(0,100)
+r=Rectangle(p1,p2)
+r.addUser(User(1,5,5))
+r.addUser(User(2,5,60))
+r.addUser(User(3,60,65))
+r.addUser(User(4,63,62))
+r.addUser(User(5,99,5))
+r.addUser(User(6,89,10))
+r.addUser(User(7,76,16))
+rq=RectangleQueue()
+rq.put(r)
+n=Rectangle(Coord(100,0),Coord(0,100))
+urq=UniqueRectangleQueue()
+t=Rectangle(Coord(100,1),Coord(0,100))
+s=Rectangle(Coord(1,0),Coord(0,1))
+urq.put(r)
+allUsers=[User(n,n,n) for n in range(10)]
+
+l=[r,t,n,copy(t)]
+
+neighbors(s)
+
+
